95c68aee66fed73de309a0705a36e3aeae95f025
HamzaBenchetra <hamza.ben2zelda@gmail.com>
2018-06-01 05:23:35 +0100

    operations Admin

--- src/Model/Appartement.java
+++ src/Model/Appartement.java
@@ -3,12 +3,8 @@ package Model;
 public class Appartement {
 private int idAppart,idBatiment,etage;
 private double prix;
-<<<<<<< HEAD
 private String description;
-private String type,nomLocal;
-=======
 private String type,nomLocal,img1;
->>>>>>> 732692019b55b8529fa800596d6adca0ec6f707d
 public int getIdAppart() {
 	return idAppart;
 }
@@ -45,7 +41,6 @@ public String getNomLocal() {
 public void setNomLocal(String nomLocal) {
 	this.nomLocal = nomLocal;
 }
-<<<<<<< HEAD
 public String getDescription() {
 	return description;
 }
@@ -59,12 +54,10 @@ public String toString() {
 }
 
 
-=======
 public String getImg1() {
 	return img1;
 }
 public void setImg1(String img1) {
 	this.img1 = img1;
 }
->>>>>>> 732692019b55b8529fa800596d6adca0ec6f707d
 }
